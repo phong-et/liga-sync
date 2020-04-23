3923cc6dbb6621ba103cfa184401f8b3530a423f
Phillip <phillip7.et@gmail.com>
2020-04-23 15:44:58 +0700

    add cli-process bar

--- sml.js
+++ sml.js
@@ -15,9 +15,10 @@ let cfg = require('./switch.cfg'),
 		'Accept-Encoding': 'gzip, deflate'
 	},
 	page = "/pgajax.axd?T=SyncImages",
-	isLog = false;
-const TIME_DELAY_EACH_DOWNLOADING_FILE = 1000;
+	isLog = true,
+	cliProgress = require('cli-progress');
 
+const TIME_DELAY_EACH_DOWNLOADING_FILE = 1000;
 async function saveFile(fileName, content) {
 	return new Promise((resolve, reject) => {
 		fs.writeFile(fileName, content, function (err) {
@@ -37,7 +38,7 @@ async function getPaths(host, stringSplit) {
 				return body.replace(/\\/g, "/");
 			}
 		}
-	//log("Get Paths :%s", url);
+	if (isLog) log("Get Paths: %s", url);
 	var paths = await rp(options)
 	paths = JSON.parse(paths);
 	paths = formatPath(paths, stringSplit);
@@ -75,6 +76,20 @@ async function fetchTextFile(url) {
 		log(`Message=${error.message.substring(0, 3)} ==> fetchTextFile:${url}`)
 	}
 }
+
+async function fetchImage(url, fullFileName) {
+	return new Promise((resolve) => {
+		let writeStream = fs.createWriteStream(fullFileName);
+		request(url)
+			.on('error', err => {
+				log('404 ==> fetchTextFile: %s', url)
+				log(err)
+			})
+			.pipe(writeStream)
+		writeStream.on('finish', resolve);
+	});
+}
+
 async function downloadFile(pathImage, host, syncFolder) {
 	//log('pathImage:%s', pathImage)
 	let url = cfg.protocol + host + '/' + pathImage,
@@ -142,6 +157,14 @@ async function getDHNumber(whiteLabelName) {
 		log(error)
 	}
 }
+async function getDomain(whiteLabelName) {
+	try {
+		let result = await getSwitchCfg()
+		return result['Clients'][whiteLabelName.toUpperCase()]['defaultDomain']
+	} catch (error) {
+		log(error)
+	}
+}
 // save image to Images_WLs\Images_<WhiteLabelName>
 async function syncImagesWL(whiteLabelName) {
 	whiteLabelName = whiteLabelName.toUpperCase()
@@ -160,23 +183,14 @@ async function syncImagesWLs(index, whiteLabelNames, next) {
 	else
 		next()
 }
-
-const fetchImage = async (url, fullFileName) => {
-	return new Promise((resolve) => {
-		let writeStream = fs.createWriteStream(fullFileName);
-		request(url)
-			.on('error', err => {
-				log('404 ==> fetchTextFile: %s', url)
-				log(err)
-			})
-			.pipe(writeStream)
-		writeStream.on('finish', resolve);
-	});
-}
 async function downloadFilesSync(imagePaths, host, syncFolder) {
-	log(imagePaths)
+	const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+	let percent = 0
+	bar1.start(imagePaths.length, 0);
 	for (const imagePath of imagePaths) {
-		log(imagePath)
+		//log(imagePath)
+		percent = percent + 1
+		bar1.update(percent);
 		let url = cfg.protocol + host + '/' + imagePath,
 			rootFolderImages = cfg.rootFolderImages,
 			fileName = imagePath.split('/').slice(-1)[0],
@@ -203,16 +217,19 @@ async function downloadFilesSync(imagePaths, host, syncFolder) {
 				break;
 		}
 	}
+	bar1.stop();
 	log("Downloaded %s files to %s folder", imagePaths.length, syncFolder);
 }
 async function syncImagesWLNew(whiteLabelName) {
 	whiteLabelName = whiteLabelName.toUpperCase()
 	log('Syncing %s', whiteLabelName)
-	let host = 'www.' + whiteLabelName + '.com',
+	let domain = await getDomain(whiteLabelName)
+	host = 'www.' + domain ? domain : whiteLabelName + '.com',
 		syncFolder = 'Images_' + whiteLabelName,
 		paths = await getPaths(host, 'WebUI')
 	await downloadFilesSync(paths, host, syncFolder)
 }
+
 module.exports = {
 	getPaths: getPaths,
 	formatPath: formatPath,
@@ -223,5 +240,6 @@ module.exports = {
 	getDHNumber: getDHNumber,
 	syncImagesWL: syncImagesWL,
 	syncImagesWLs: syncImagesWLs,
-	syncImagesWLNew: syncImagesWLNew
+	syncImagesWLNew: syncImagesWLNew,
+	getDomain: getDomain
 };
