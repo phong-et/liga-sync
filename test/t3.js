ab46c503dd6cb46bcb988caf617bcb4e92d64eec
Phillip <phillip7.et@gmail.com>
2020-05-18 16:04:20 +0700

    add test/ and log.txt

---
+++ test/t3.js
@@ -0,0 +1,6 @@
+// test 
+(async function () {
+    let sync = require('../sml'), log = console.log
+    //sync.fetchAllImagePathsFromLocal('RAJAELANG')
+    sync.fetchAllImagePathsFromLive('RAJAELANG')
+})()
